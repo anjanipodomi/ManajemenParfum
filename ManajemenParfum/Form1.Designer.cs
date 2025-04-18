f035ff83ceb9d607df87f1d60d9345b257461763
anjanipodomi <anjanidihapsari@gmail.com>
2025-04-18 12:18:56 +0700

    Add project files.

---
+++ ManajemenParfum/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ManajemenParfum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.ID_Pelanggan = new System.Windows.Forms.Label();
+            this.Nama = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dgvPelanggan = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPelanggan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(284, 116);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(282, 26);
+            this.txtID.TabIndex = 0;
+            this.txtID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(284, 200);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(282, 26);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(284, 284);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(282, 26);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // ID_Pelanggan
+            // 
+            this.ID_Pelanggan.AutoSize = true;
+            this.ID_Pelanggan.Location = new System.Drawing.Point(97, 107);
+            this.ID_Pelanggan.Name = "ID_Pelanggan";
+            this.ID_Pelanggan.Size = new System.Drawing.Size(111, 20);
+            this.ID_Pelanggan.TabIndex = 3;
+            this.ID_Pelanggan.Text = "ID_Pelanggan";
+            this.ID_Pelanggan.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Location = new System.Drawing.Point(101, 200);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(51, 20);
+            this.Nama.TabIndex = 4;
+            this.Nama.Text = "Nama";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(101, 290);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(48, 20);
+            this.Email.TabIndex = 5;
+            this.Email.Text = "Email";
+            this.Email.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(683, 91);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 23);
+            this.btnTambah.TabIndex = 6;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(683, 168);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 7;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(683, 242);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(683, 317);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // dgvPelanggan
+            // 
+            this.dgvPelanggan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPelanggan.Location = new System.Drawing.Point(78, 387);
+            this.dgvPelanggan.Name = "dgvPelanggan";
+            this.dgvPelanggan.RowHeadersWidth = 62;
+            this.dgvPelanggan.RowTemplate.Height = 28;
+            this.dgvPelanggan.Size = new System.Drawing.Size(712, 255);
+            this.dgvPelanggan.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 654);
+            this.Controls.Add(this.dgvPelanggan);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.ID_Pelanggan);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPelanggan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label ID_Pelanggan;
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dgvPelanggan;
+    }
+}
+
